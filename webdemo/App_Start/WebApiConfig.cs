31b2a177f5e2e2a67384eafe2a0654b3557b5daa
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2016-01-05 15:01:49 -0600

    initial push, with code for webapi and html having Get, Get(id), Post and Put

---
+++ webdemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace webdemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.Clear(); // clear in the event there's defaults set by .net
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ webdemo/Controllers/ProductController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using webdemo.Models;
+
+namespace webdemo.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            IHttpActionResult toReturn = null;
+            List<Product> list = new List<Product>();
+
+            list = CreateMockData();
+
+            if (list.Count > 0)
+            {
+                toReturn = Ok(list);
+            }
+            else
+            {
+                toReturn = NotFound();
+            }
+
+            return toReturn;
+        }
+
+
+        // GET: api/Product/5
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            IHttpActionResult toReturn = null;
+            List<Product> list = new List<Product>();
+
+            Product prod = new Product();
+
+            list = CreateMockData();
+            prod = list.Find(p => p.ProductId == id);
+
+            if (prod == null)
+            {
+                toReturn = NotFound();
+            }
+            else
+            {
+                toReturn = Ok(prod);
+            }
+
+            return toReturn;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(Product product)
+        {
+            IHttpActionResult ret = null;
+
+            if (Add(product))
+            {
+                ret = Created<Product>(Request.RequestUri + product.ProductId.ToString(), product);
+            }
+            else
+            {
+                ret = NotFound();
+            }
+
+            return ret;
+        }
+
+
+        [HttpPut]
+        public IHttpActionResult Put(int id, Product product)
+        {
+            IHttpActionResult ret = null;
+
+            if (Update(product))
+            {
+                ret = Ok(product);
+            }
+            else
+            {
+                ret = NotFound();
+            }
+
+            return ret;
+        }
+
+        // mock method to simulate adding a new product
+        private bool Add(Product product)
+        {
+            int newId = 0;
+            List<Product> list = new List<Product>();
+
+            list = CreateMockData();
+
+            newId = list.Max(p => p.ProductId);
+            newId++;
+            product.ProductId = newId;
+            list.Add(product);
+
+            // todo: change to 'false' to test NotFound()
+            return true;
+        }
+
+        // mock method to simulate an update to a record
+        private bool Update(Product product)
+        {
+            return true;
+        }
+
+        private List<Product> CreateMockData()
+        {
+            List<Product> toReturn = new List<Product>();
+
+            toReturn.Add(new Product
+            {
+                ProductId = 1,
+                ProductName = "Extending bootstrap with css, javascript and jquery",
+                IntroductionDate = Convert.ToDateTime("06/11/2015"),
+                Url = "http://bit.ly/1I8ZqZg"
+            });
+
+            toReturn.Add(new Product
+            {
+                ProductId = 2,
+                ProductName = "Build your own Bootstrap Business",
+                IntroductionDate = Convert.ToDateTime("01/29/2015"),
+                Url = "http://bit.ly/1SNzC0i"
+            });
+
+            toReturn.Add(new Product
+            {
+                ProductId = 3,
+                ProductName = "Building mobile web sites using web forms, bootstrap and html5",
+                IntroductionDate = Convert.ToDateTime("08/28/2015"),
+                Url = "http://bit.ly/1j2dcrj"
+            });
+
+            return toReturn;
+        }
+    }
+}
+
+
+
+
+//       // POST: api/Product
+//       public void Post([FromBody]string value)
+//       {
+//       }
+
+//       // PUT: api/Product/5
+//       public void Put(int id, [FromBody]string value)
+//       {
+//       }
+
+//       // DELETE: api/Product/5
+//       public void Delete(int id)
+//       {
+//       }
